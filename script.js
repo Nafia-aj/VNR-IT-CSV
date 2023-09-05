b55d69ed8c1942bedba99b014ed0bcfaf041870f
Nafia-aj <143184864+Nafia-aj@users.noreply.github.com>
2023-09-06 01:19:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+(function () {
+    emailjs.init("N2e2SbIggu5DgyLvA");
+})();
+
+function sendEmails(event) {
+    event.preventDefault();
+
+    var subject = document.getElementById("subject").value;
+    var content = document.getElementById("content").value;
+    var csvFile = document.getElementById("csvFile").files[0];
+
+    if (!csvFile) {
+        alert("Please select a CSV file.");
+        return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var csvText = e.target.result;
+        console.log("CSV Text:", csvText);
+
+        Papa.parse(csvText, {
+            delimiter: ",",
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true,
+            complete: function (results) {
+                if (results.errors.length > 0) {
+                    console.error("Error parsing CSV:", results.errors);
+                    return;
+                }
+                console.log("Parsed CSV data:", results.data);
+                console.log("Errors:", results.errors);
+                
+                const serviceID = "service_th478dp";
+                const templateID = "template_mdo1sji";
+
+                results.data.forEach(function (row) {
+                    if (row.email) {
+                        var params = {
+                            email: row.email,
+                            subject: subject,
+                            content: content,
+                        };
+
+                        emailjs.send(serviceID, templateID, params).then((res) => {
+                            console.log("Message sent successfully to " + row.email);
+                        }).catch((err) => {
+                            console.error(`Error sending message to ${row.email}:`, err);
+                        });
+                    }
+                });
+
+                document.getElementById('subject').value = '';
+                document.getElementById('content').value = '';
+                document.getElementById('csvFile').value = ''; // Clear file input
+            }
+        });
+    };
+
+    reader.readAsText(csvFile);
+}
+
+
+
+
+
+
+
+
